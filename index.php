a9c9ffd75e198ec9acfb88858e704ec41c306286
Fardeen Bukhari <77046990+fardeenbukhari@users.noreply.github.com>
2023-02-09 15:20:03 +0530

    Update index.php

--- index.php
+++ index.php
@@ -24,7 +24,7 @@ $result = curl_exec($ch);
 $api = json_decode($result,JSON_PRETTY_PRINT);
 foreach($api['items'] as $item){
     $link = $item['contentDetails']['upload']['videoId'];
-    echo '<iframe width="560" height="315" src="https://www.youtube.com/embed/'.$link.'" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen style="padding:1vh;margin:2vh;">'.'</iframe>';
+    echo $link;
 }
 
 ?>
